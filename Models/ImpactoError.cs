817c22047a70649c229ee93bea2ac5e9634542ba
Jose Alexander Ríos Trespalacios <joserios055@gmail.com>
2024-05-29 09:08:36 -0500

    Create ImpactoError.cs

---
+++ Models/ImpactoError.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Qualitas.Models
+{
+    public class ImpactoError
+    {
+            public string Impacto { get; set; }
+            public string Errores { get; set; }
+            public string Porcentaje { get; set; }
+            public string Fecha { get; set; }
+            public string Mes { get; set; }
+       
+    }
+}
