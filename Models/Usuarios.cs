22a573817bf8c8657ae8e7bce2ecc081eb135136
Jose Alexander Ríos Trespalacios <joserios055@gmail.com>
2024-05-29 09:07:49 -0500

    Create Usuarios.cs

---
+++ Models/Usuarios.cs
@@ -0,0 +1,17 @@
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Qualitas.Models
+{
+    public class Usuarios
+    {
+        public int Id { get; set; }
+        public string Usuario { get; set; }
+        public string Perfil { get; set; }
+        public string Correo { get; set; }
+        public string Nombre { get; set; }
+        public string Area { get; set; }
+    }
+}
