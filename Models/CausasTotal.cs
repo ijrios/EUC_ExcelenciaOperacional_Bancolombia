9c678d946234d4beb007f4dc4e30746c9c1e9252
Jose Alexander Ríos Trespalacios <joserios055@gmail.com>
2024-06-07 17:34:45 -0500

    Create CausasTotal.cs

---
+++ Models/CausasTotal.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Qualitas.Models
+{
+    public class CausasTotal
+    {
+        public string Id { get; set; }
+        public string Area { get; set; }
+
+        public string Tipo { get; set; }
+        public string Causa { get; set; }
+    }
+}
