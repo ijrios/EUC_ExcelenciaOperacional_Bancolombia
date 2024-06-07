f1059d475e79ad18c9198190be6a41ca6ed50686
Jose Alexander Ríos Trespalacios <joserios055@gmail.com>
2024-06-07 17:35:13 -0500

    Create ErrorArea.cs

---
+++ Models/ErrorArea.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Qualitas.Models
+{
+    public class ErrorArea
+    {
+        public string Area { get; set; }
+        public string Errores { get; set; }
+        public string Porcentaje { get; set; }
+        public string Fecha { get; set; }
+        public string Mes { get; set; }
+    }
+}
